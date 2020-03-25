34e2b0f494a0363d411ee9b3cd4828187e0c3726
mingzhi <momomingzhi@gmail.com>
2020-03-25 23:33:00 +0900

    TodoList 만들기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
+import TodoList from './components/TodoLists'
 const App = ()=>{
   return( <TodoTemplate>
             <TodoInsert />
+            <TodoList />
           </TodoTemplate>
   );
 };
