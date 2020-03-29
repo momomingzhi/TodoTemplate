84107f20b3fc136d7065897021e51ec4b3726377
myeongji_jang <myeongji_jang@tmax.co.kr>
2020-03-29 19:10:52 +0900

    할일 리스트 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
-import TodoList from './components/TodoLists'
 const App = ()=>{
+  const [todos,setTodos]=useState([
+    {
+      id:1,
+      text:'리액트의 기초 알아보기',
+      checked:true,
+    },
+    {
+      id:2,
+      text:'컴포넌트 스타일링 해보기',
+      checked:true,
+    },
+    {
+      id:3,
+      text:'일정 관리 앱 만들어 보기',
+      checked:false,
+    },
+  ]);
   return( <TodoTemplate>
             <TodoInsert />
-            <TodoList />
           </TodoTemplate>
   );
 };
